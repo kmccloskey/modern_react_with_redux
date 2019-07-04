a45bc917e2c8b67e92a90bfc7d4bfccbef5fef74
Kyle McCloskey <mccloskeykyle@gmail.com>
2019-07-04 14:19:15 -0400

    end of jsx tutorial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Import the react and react-dom libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Create a react component
+
+const App = () => {
+  const buttonText = { text: "Click Me" };
+  return (
+    <div>
+      <label class="label" htmlFor="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
